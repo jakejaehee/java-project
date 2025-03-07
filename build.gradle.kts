f8cc5421fca6fe2cacb382fdaffb0f65dc8d257f
jakejaehee <jakejaehee@gmail.com>
2025-03-08 00:48:39 +0900

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-loader-tools")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/org/example/BracketChecker.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.util.Stack;
+
+public class BracketChecker {
+    private Stack stack = new Stack();
+
+    private static final String OPEN_BRACKETS = "({<[";
+    private static final String CLOSE_BRACKETS = ")}>]";
+
+    private boolean isOpenBracket(char bracket) {
+        return OPEN_BRACKETS.contains(String.valueOf(bracket));
+    }
+
+    private boolean isCloseBracket(char bracket) {
+        return CLOSE_BRACKETS.contains(String.valueOf(bracket));
+    }
+
+    private boolean isMatched(char openBracket, char closeBracket) {
+        int openIdx = OPEN_BRACKETS.indexOf(openBracket);
+        int closeIdx = CLOSE_BRACKETS.indexOf(closeBracket);
+        return openIdx == closeIdx && openIdx != -1;
+    }
+
+    public boolean isCorrectBracketString(String brackets) {
+        for (int i = 0; i < brackets.length(); i++) {
+            char bracket = brackets.charAt(i);
+
+            if (isOpenBracket(bracket)) {
+                stack.push(bracket);
+            } else if (isCloseBracket(bracket)) {
+                if (stack.size() == 0) {
+                    return false;
+                }
+                char openBracket = (char) stack.pop();
+                if (isMatched(openBracket, bracket) == false) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String brackets = "{}[{<>fdaf}]";
+        BracketChecker bracketChecker = new BracketChecker();
+        if (bracketChecker.isCorrectBracketString(brackets)) {
+            System.out.println(brackets + " is correct");
+        } else {
+            System.out.println(brackets + " is incorrect");
+        }
+    }
+}
+
